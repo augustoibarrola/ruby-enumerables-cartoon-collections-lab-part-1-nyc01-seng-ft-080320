fe917ccfc21537221846cbedbbf9b106759b212a
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-22 15:08:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+  array.each do |x|
+    "Hello #{x}!"
+end
 end
 
 def list_dwarves(array)
